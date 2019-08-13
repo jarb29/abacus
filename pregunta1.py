51441c7dd0588d9db90aff0de0fe0a134f0dcb0b
Cristobal Mackenzie <c.mack04@gmail.com>
2019-08-13 11:41:29 -0400

    .

---
+++ pregunta1.py
@@ -0,0 +1,16 @@
+# Implementa la función fib(n) que retorna el n-ésimo número de la serie
+# de Fibonacci:
+# fib(1) = 1
+# fib(2) = 1
+# fib(n) = fib(n - 1) + fib(n - 2), para todo n > 2.
+
+
+def fib(n):
+    pass
+
+
+if __name__ == "__main__":
+    # Prueba de la función: imprime los primeros 10 números en una línea
+    # separados por coma y espacio.
+    prueba_fib = ''
+    print(prueba_fib)
