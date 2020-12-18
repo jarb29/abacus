807df646f0a207484b2d7f720df9192457f56829
jarb29 <jarb29@gmail.com>
2020-12-17 23:33:20 -0300

    type: Interviee Body: Version recursiva sin terminar

--- pregunta2.py
+++ pregunta2.py
@@ -16,6 +16,8 @@ node3 = Node('Bonos US', amount=100000.0, parent=node1)
 node4 = Node('Bonos Chile', amount=200000.0, parent=node1)
 node5 = Node('Acciones US', amount=500000.0, parent=node2)
 node6 = Node('Acciones Chile', amount=200000.0, parent=node2)
+node7 = Node('Acciones Chiiiiiiiiile', amount=800000.0, parent=node3)
+node8 = Node('Acciones Chiiiiiiiiileeeeeeeeeee', amount=800000.0, parent=node7)
 
 """
 Implementa la función print_tree, que debe recibir como único argumento el nodo
@@ -44,7 +46,43 @@ Total: 1000000.0
 # def print_tree(root_node):
 #   print_node(root_node, 0)
 
+
 ### Version Iterativa 
+# def print_tree(root_node):
+#   current_node = root_node
+#   nivel = 0
+#   print("{} {}: {} ".format(" "*nivel, current_node.name, current_node.amount))
+ 
+#   for child in current_node.children:
+#     nivel = 2
+#     print("{} {}: {} ".format(" "*nivel, child.name, child.amount))
+#     while child.children:
+#       current_node = child
+#       nivel +=2
+      # for child in child.children:
+        # print("{} {}: {} ".format(" "*nivel, child.name, child.amount))
+        # current_node = child
+
+
+
+def print_tree(root_node):
+
+  current_node = root_node
+  print("{}: {} ".format(current_node.name, current_node.amount))
+  nivel = 0
+  while current_node.children:
+    nivel = 2
+    for child in current_node.children:
+      
+      print("{} {}: {} ".format(" "*nivel, child.name, child.amount))
+      current_node = child
+      nivel += 2
+      # for child in child.children:
+        
+      #   print("{} {}: {} ".format(" "*nivel, child.name, child.amount))
+      #   current_node = child
+
+
 
 
 
